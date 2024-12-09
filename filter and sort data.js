01dbb3ea6aa588bd6cbbc7aa83ba70276beda4a6
Krishna Sharma <krishnapurohit417400@gmail.com>
2024-12-09 21:57:20 +0530

    Create filter and sort data.js

---
+++ filter and sort data.js
@@ -0,0 +1,20 @@
+You are given an array of student objects. Each student object contains a name and marks. Write a function processStudents that performs the following steps:
+
+Filter the students to create a new array of students who scored above 60 marks.
+Sort the filtered array in descending order of marks.
+Map the sorted array to extract only the names of the students.
+Return the array of sorted names.
+Example:
+
+Input: [ { name: "Alice", marks: 58 },
+
+{ name: "Bob", marks: 85 },
+
+{ name: "Charlie", marks: 92 },
+
+{ name: "David", marks: 45 } ]
+
+Output: ["Charlie", "Bob"]
+
+Submission Guidelines:
+Please submit your Masai GitHub repository link.
